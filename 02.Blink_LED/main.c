7ddd8ced1b6b556f6dd41cacaa637a858b02edb4
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2021-01-06 08:35:57 -0800

    Blink Port

---
+++ 02.Blink_LED/main.c
@@ -0,0 +1,18 @@
+#include<reg52.h>            
+int i,j;
+void Delay(void);          
+
+void main (void)
+{
+    while(1)                 
+    {
+			P0 = 0;
+			for(i=0;i<10000;i++)
+			{
+			}
+			P0=1;
+			for(i=0;i<10000;i++)
+			{
+			}
+    }
+}
